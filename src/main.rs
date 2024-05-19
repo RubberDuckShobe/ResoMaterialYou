2cc79f8f36be01ac58d2408052a324655101adcf
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2024-05-19 15:45:49 +0200

    Add dark/light theme option and custom preset colors

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,99 @@ use axum::{
     routing::get,
     Router,
 };
-use material_colors::{color::Argb, theme::ThemeBuilder};
+use material_colors::{
+    color::Argb,
+    theme::{CustomColor, ThemeBuilder},
+};
 use serde::Deserialize;
-use tower_http::trace::TraceLayer;
 use std::str::FromStr;
+use tower_http::trace::TraceLayer;
 use tracing::{error, info};
+
+#[derive(Debug, Deserialize)]
+enum ThemeType {
+    Dark,
+    Light,
+}
 
 #[derive(Deserialize)]
 struct PaletteQuery {
     base_color: String,
+    theme_type: ThemeType,
 }
 
 async fn get_palette(pagination: Query<PaletteQuery>) -> Result<String, AppError> {
     let query: PaletteQuery = pagination.0;
 
-    info!("Generating theme with color string {:?}", &query.base_color);
+    info!(
+        "Generating {:?} theme with color string {:?}",
+        &query.theme_type, &query.base_color
+    );
 
-    let theme = ThemeBuilder::with_source(Argb::from_str(&query.base_color)?).build();
+    // Define some fixed (but still blended) colors to make people's lives easier
+    let red = CustomColor {
+        value: Argb::from_str("#FF0000")?,
+        name: "red".to_string(),
+        blend: true,
+    };
+    let green = CustomColor {
+        value: Argb::from_str("#00FF00")?,
+        name: "green".to_string(),
+        blend: true,
+    };
+    let blue = CustomColor {
+        value: Argb::from_str("#0000FF")?,
+        name: "blue".to_string(),
+        blend: true,
+    };
+    let custom_colors: Vec<CustomColor> = vec![red, green, blue];
+    let theme = ThemeBuilder::with_source(Argb::from_str(&query.base_color)?)
+        .custom_colors(custom_colors)
+        .build();
 
-    let final_string = theme
-        .schemes
-        .light
-        .into_iter()
-        .map(|x| x.1.to_hex())
+    let base_theme_string = match query.theme_type {
+        ThemeType::Dark => theme
+            .schemes
+            .dark
+            .into_iter()
+            .map(|x| x.1.to_hex())
+            .collect::<Vec<_>>()
+            .join(""),
+        ThemeType::Light => theme
+            .schemes
+            .light
+            .into_iter()
+            .map(|x| x.1.to_hex())
+            .collect::<Vec<_>>()
+            .join(""),
+    };
+
+    let custom_colors_string = theme
+        .custom_colors
+        .iter()
+        .map(|x| match query.theme_type {
+            ThemeType::Dark => format!(
+                "{}:{}{}{}{}",
+                x.color.name,
+                x.dark.color.to_hex(),
+                x.dark.color_container.to_hex(),
+                x.dark.on_color.to_hex(),
+                x.dark.on_color_container.to_hex()
+            ),
+            ThemeType::Light => format!(
+                "{}:{}{}{}{}",
+                x.color.name,
+                x.light.color.to_hex(),
+                x.light.color_container.to_hex(),
+                x.light.on_color.to_hex(),
+                x.light.on_color_container.to_hex()
+            ),
+        })
         .collect::<Vec<_>>()
         .join("");
-    
+
+    let final_string = format!("{};{}", base_theme_string, custom_colors_string);
+
     info!("Generated theme: {:?}", final_string);
 
     Ok(final_string)
@@ -61,7 +128,7 @@ async fn main() -> shuttle_axum::ShuttleAxum {
                         .map(|matched_path| matched_path.as_str());
 
                     tracing::debug_span!("request", %method, %uri, matched_path)
-                })
+                }),
         );
 
     Ok(router.into())
